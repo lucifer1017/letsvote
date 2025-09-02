ce6f35c736fc14fcde5aeb30280e58694325683e
lucifer1017 <lucifergrey1017@gmail.com>
2025-09-02 14:01:31 +0530

    highly imp test case , using getAddress from viem, not hardhat-viem

--- hardhat_ex/test/Whitelist.ts
+++ hardhat_ex/test/Whitelist.ts
@@ -1,6 +1,7 @@
 import assert from "node:assert/strict";
 import { describe,it } from "node:test";
 import { network } from "hardhat";
+import { getAddress } from "viem";
 
 describe("Whitelist contract",async ()=>{
     const {viem}=await network.connect();
@@ -9,7 +10,7 @@ describe("Whitelist contract",async ()=>{
     it("The owner is the deployer",async ()=>{
         const whitelist=await viem.deployContract("Whitelist");
         const contractOwner=await whitelist.read.owner();
-        assert.equal(owner.account.address,contractOwner.toLocaleLowerCase());
+        assert.equal(contractOwner.toLocaleLowerCase(),owner.account.address,"Owner is not the deployer");
     });
 
     it("Only owner should be able to add to Whitelist",async ()=>{
@@ -26,7 +27,41 @@ describe("Whitelist contract",async ()=>{
             "Function call should have been reverted"
         );
     });
+    it("Should emit an AddressWhitelisted event",async ()=>{
+        const whitelist=await viem.deployContract("Whitelist");
 
-    
+        await viem.assertions.emitWithArgs(
+            whitelist.write.addToWhitelist([otherAccount.account.address]),
+            whitelist,
+            "AddressWhitelisted",
+            [getAddress(otherAccount.account.address)]
+        );
+    });
+    it("addNums function, owner should be able to add to nums array",async ()=>{
+        const whitelist=await viem.deployContract("Whitelist");
+
+        await whitelist.write.addToWhitelist([thirdAccount.account.address]);
+        const isListed= await whitelist.read.isWhitelisted([thirdAccount.account.address]);
+
+        assert.equal(
+             isListed,
+             true,
+             "Owner could not add the user to isWhitelisted"   
+        );
+        
+    });
+    it("Should revert if trying to add already whitelisted user address",async ()=>{
+        const whitelist=await viem.deployContract("Whitelist");
+
+        await whitelist.write.addToWhitelist([otherAccount.account.address]);
+
+        await assert.rejects(
+            whitelist.write.addToWhitelist([otherAccount.account.address]),
+            (err:Error)=>err.message.includes("User is already Whitelisted"),
+            "Unable to revert adding already whitelisted user address"
+        );
+    });
+
+
 
 });
